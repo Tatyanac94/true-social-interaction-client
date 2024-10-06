784f0cc2880e539297f985aeb0375747273c55f0
Tatyanac94 <ms.cuttino94@gmail.com>
2024-10-05 23:44:10 -0400

    removed file

--- src/app/api/welcome/route.js
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-    return new Response(JSON.stringify({ message: 'Welcome to Next.js!' }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  
