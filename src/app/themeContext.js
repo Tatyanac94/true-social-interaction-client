ec7a90383307e28a4963bd29cfd97ddfe9a28164
Tatyanac94 <ms.cuttino94@gmail.com>
2024-10-05 21:51:32 -0400

    added shadows to the posts

--- src/app/themeContext.js
+++ src/app/themeContext.js
@@ -1,41 +1,3 @@
-// 'use client';
-
-// import React, { createContext, useContext, useState, useEffect } from 'react';
-
-// const ThemeContext = createContext();
-
-// export const ThemeProvider = ({ children }) => {
-//   const [theme, setTheme] = useState('light');
-
-//   useEffect(() => {
-//     const savedTheme = localStorage.getItem('theme') || 'light';
-//     setTheme(savedTheme);
-//     document.body.className = savedTheme; // Set the initial body class
-//   }, []);
-
-//   const toggleTheme = () => {
-//     const newTheme = theme === 'light' ? 'dark' : 'light';
-//     setTheme(newTheme);
-//     localStorage.setItem('theme', newTheme);
-//     document.body.className = newTheme; // Update the body class
-//   };
-
-//   return (
-//     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
-
-// export const useTheme = () => useContext(ThemeContext);
-
-
-
-
-
-
-
-
 'use client';
 
 import React, { createContext, useContext, useState, useEffect } from 'react';
@@ -48,14 +10,14 @@ export const ThemeProvider = ({ children }) => {
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme') || 'light';
     setTheme(savedTheme);
-    document.body.className = savedTheme; // Set the initial body class
+    document.body.className = savedTheme; 
   }, []);
 
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
     localStorage.setItem('theme', newTheme);
-    document.body.className = newTheme; // Update the body class
+    document.body.className = newTheme; 
   };
 
   return (
